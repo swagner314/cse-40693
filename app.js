be1a135ae781c83acd2cfe02ef1acb490a661725
Steven Wagner <swagner314@nd.edu>
2020-09-16 02:35:32 -0400

    Polishing navbar and structured the files for the rest of the feature

--- app.js
+++ app.js
@@ -1,42 +1,78 @@
 angular.module('app', ['ngMaterial', 'ngMessages']);
 
 /*--------------------- Home Component ---------------------*/
+const home = {
+    templateUrl: './home/home.html',
+    controller: 'HomeController'
+}
+
+angular.module('app').component('home', home);
+
+angular.module('app').controller('HomeController', ['GetData', '$scope', '$timeout', function (GetData, $scope, $timeout) {
+    $scope.currentTab = 'home';
+    $scope.$on("changedTab", function(event, newTab){
+        $timeout(function() {
+            $scope.currentTab = newTab;
+        })
+    });
+}]);
+
+/*--------------------- Welcome Component ---------------------*/
+const welcome = {
+    templateUrl: './welcome/welcome.html',
+    controller: 'WelcomeController'
+}
+
+angular.module('app').component('welcome', welcome);
+
+angular.module('app').controller('WelcomeController', ['GetData', function (GetData) {
+    this.exampleVariable = 'welcome comp';
+}]);
+
+/*--------------------- Navbar Component ---------------------*/
 const navbar = {
     templateUrl: './navbar/navbar.html',
     controller: 'NavbarController'
 }
 
-// Home Component with Routing (Routed / Stateful)
 angular.module('app').component('navbar', navbar);
 
-// Home Controller with dependency injection using the array method
-angular.module('app').controller('NavbarController', ['ExampleService', function (ExampleService) {
-    this.exampleVariable = 'Test String';
-    this.isActive = true;
-    this.logoClr = 'white'
-    console.log(this)
+angular.module('app').controller('NavbarController', ['$rootScope', function ($rootScope) {
+    this.over = false;
+    this.activeTab = 'home';
+    
+    this.changeTab = function (str) {
+        $rootScope.$broadcast('changedTab', str);
+        this.activeTab = str;
+    }
 }]);
-/*--------------------- Home Component ---------------------*/
 
-/*--------------------- Settings Component ---------------------*/
-const settings = {
-    templateUrl: '',
-    controller: 'SettingsController'
+/*--------------------- Puzzle1 Component ---------------------*/
+const puzzle1 = {
+    templateUrl: './puzzle1/puzzle1.html',
+    controller: 'Puzzle1Controller'
 }
 
-// Settings Component with Routing (Routed / Stateful)
-angular.module('app').component('settings', settings)
+angular.module('app').component('puzzle1', puzzle1)
 
-// Settings Controller with dependency injection using $inject method
-function SettingsController(ExampleService) {
+angular.module('app').controller('Puzzle1Controller', ['GetData', function (GetData) {
+    this.exampleVariable = '';
+}]);
 
+/*--------------------- Puzzle2 Component ---------------------*/
+const puzzle2 = {
+    templateUrl: './puzzle2/puzzle2.html',
+    controller: 'Puzzle2Controller'
 }
-SettingsController.$inject = ['ExampleService'];
-angular.module('app').controller('SettingsController', SettingsController);
-/*--------------------- Settings Component ---------------------*/
 
-/*--------------------- Example Service ---------------------*/
-function ExampleService($http) {
+angular.module('app').component('puzzle2', puzzle2)
+
+angular.module('app').controller('Puzzle2Controller', ['GetData', function (GetData) {
+    this.exampleVariable = '';
+}]);
+
+/*--------------------- GetData Service ---------------------*/
+function GetData($http) {
     this.getData = getData;
     
     function getData() {
@@ -46,6 +82,20 @@ function ExampleService($http) {
         })
     }
 }
-angular.module('app').service('ExampleService', ExampleService)
-/*--------------------- Example Service ---------------------*/
+angular.module('app').service('GetData', GetData)
 
+/*--------------------- SwitchTab Service ---------------------
+function SwitchTab() {
+    this.setTab = setTab;
+    this.curTab = 'home';
+    
+    function setTab(str) {
+        this.curTab = str;
+        return Promise.resolve(str);
+    }
+    
+    function getTab() {
+        return this.curTab;
+    }
+}
+angular.module('app').service('SwitchTab', SwitchTab)*/
