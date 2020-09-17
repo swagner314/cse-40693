fc63bd852f802a72231d590e5aada064354ee719
Katherine McHugh <katiemchugh5@gmail.com>
2020-09-16 20:38:23 -0400

    Puzzle 2 and some general updates

--- app.js
+++ app.js
@@ -62,6 +62,8 @@ angular.module('app').component('puzzle1', puzzle1)
 
 angular.module('app').controller('Puzzle1Controller', ['GetData', function (GetData) {
     const $ctrl = this
+    
+    // read data from songs.json
     GetData.getData('./puzzle1/songs.json').then(function (result) {
         $ctrl.songs = result.data.values;
     }, function (error){
@@ -94,6 +96,8 @@ angular.module('app').component('puzzle2', puzzle2)
 
 angular.module('app').controller('Puzzle2Controller', ['GetData', function (GetData) {
     const $ctrl = this
+    
+    // read data from fruits.json
     GetData.getData('./puzzle2/fruits.json').then(function (result) {
         $ctrl.fruits = result.data.values;
         console.log(JSON.stringify($ctrl.fruits))
@@ -101,24 +105,53 @@ angular.module('app').controller('Puzzle2Controller', ['GetData', function (GetD
         console.log(error);
     })
     
+    // update the order of the fruits when an up or down arrow is clicked
     this.updateOrder = function (event) {
-        f = event.fruit
+        rank = event.fruit
         inc = event.isIncreasing
-        $ctrl.fruits.forEach(curFruit => {
-            if (curFruit[0] == f) {
-                console.log('same')
+        
+        for (var i = 0; i < $ctrl.fruits.length; i++) {
+            // current array of [rank, fruit_name]
+            curFruit = $ctrl.fruits[i]
+            
+            // if the passed in rank matches the current fruit's rank
+            if (curFruit[0] == rank) {
+                // if we are moving the current fruit up on the fruit list
                 if (inc) {
-                    if (curFruit[1] != 1) {
-                        // decrease rank #
-                        // increase rank # of replacement
+                    if (curFruit[0] != 1) {
+                        // increase rank of replacement fruit
+                        for (var j = 0; j < $ctrl.fruits.length; j++) {
+                            newFruit = $ctrl.fruits[j]
+                            if (newFruit[0] == rank - 1) {
+                                $ctrl.fruits[j][0] = rank
+                            }
+                        }
+                        
+                        // decrease rank of clicked fruit
+                        $ctrl.fruits[i][0] = rank - 1
                     }
                 }
+                
+                // if we are moving the current fruit down on the fruit list
                 else {
-                    if (curFruit[1] != $ctrl.fruits.length)
-                        console.log('decrease, length: ', $ctrl.fruits.length)
+                    if (curFruit[0] != $ctrl.fruits.length) {
+                        // decrease rank of replacement fruit
+                        for (var j = 0; j < $ctrl.fruits.length; j++) {
+                            newFruit = $ctrl.fruits[j]
+                            if (newFruit[0] == rank + 1) {
+                                $ctrl.fruits[j][0] = rank
+                            }
+                        }
+                        
+                        // increase rank of clicked fruit
+                        $ctrl.fruits[i][0] = rank + 1
+                    }
                 }
+                
+                // break because we have already found and dealt with the matching fruit
+                break
             }
-        })
+        }
     }
 }]);
 
