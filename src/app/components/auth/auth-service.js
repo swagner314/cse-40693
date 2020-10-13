2d14139b3f657ec056e9fa08691e58578a0f7657
Katherine McHugh <katiemchugh5@gmail.com>
2020-10-13 18:38:01 -0400

    auth updates

--- src/app/components/auth/auth-service.js
+++ src/app/components/auth/auth-service.js
@@ -1,7 +1,9 @@
 // NOTES:
-// use link in slack for completed app (plus authentication videos)
+// completed auth.service.js for firebase:
+// https://github.com/UltimateAngular/ultimate-angular-master-seed/blob/master/00-complete/app/components/auth/auth.service.js
 
-// parseplatform parse API 
+// parseplatform parse API:
+// http://parseplatform.org/Parse-SDK-JS/api/2.16.0/Parse.User.html
 
 // the things with $s starting them are things we have to replace for parse
 
